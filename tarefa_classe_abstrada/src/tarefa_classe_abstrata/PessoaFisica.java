3e9f87f90e3c41e77c19196b7fd55202be9a0f7e
Alissandra <alissandra.o.t@gmail.com>
2024-05-14 10:18:53 -0300

    atividade sobre classes abstratas

---
+++ tarefa_classe_abstrada/src/tarefa_classe_abstrata/PessoaFisica.java
@@ -0,0 +1,14 @@
+package tarefa_classe_abstrata;
+
+public class PessoaFisica extends Pessoa{
+
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ tarefa_classe_abstrada/src/tarefa_classe_abstrata/PessoaJuridica.java
@@ -0,0 +1,15 @@
+package tarefa_classe_abstrata;
+
+public class PessoaJuridica extends Pessoa{
+
+    private String cnpj;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
+
